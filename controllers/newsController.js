0e095ce16faee82c696e31c643d1b5fd19a45dc4
jransome <jamesransome@msn.com>
2018-03-28 16:11:41 +0100

    increase news cache timeout

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -2,22 +2,22 @@ const fetchUrl = require('../modules/fetchUrl');
 const cache = require('../cache');
 
 const newsController = () => {
-    let key = require('../keys').newsApi;
+    const key = require('../keys').newsApi;
     let sources = "?sources=" +
         "bbc-news," +
         "reuters," +
         "the-guardian-uk," +
         "techradar";
     let url = `https://newsapi.org/v2/top-headlines${sources}&apiKey=${key}`;
+    let cacheTimeout = 120;
 
     let get = (req, res) => {
         cache.get('news', (err, cachedNews) => {
-            // if (err) console.log('REDIS ERR: ', err);
             if (cachedNews !== null) {
                 res.json(JSON.parse(cachedNews));
             } else {
                 fetchUrl(url).then((response) => {
-                    cache.set('news', JSON.stringify(response), 'EX', 5, () => {
+                    cache.set('news', JSON.stringify(response), 'EX', cacheTimeout, () => {
                         res.json(response);
                     });
                 }, (errorMessage) => {
